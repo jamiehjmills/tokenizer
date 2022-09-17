38e41328d95f8d223ae6af4df4f6528be6e96c06
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-09-17 19:54:05 +1000

    passing params from application.properties

---
+++ src/main/java/hj/project/token/configs/TokenDb.java
@@ -0,0 +1,22 @@
+package hj.project.token.configs;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@Configuration
+@NoArgsConstructor
+@ConfigurationProperties(prefix="token")
+public class TokenDb {
+
+    String db;
+    String url;
+    String schema;
+
+}
--- src/main/java/hj/project/token/services/Tokenizer.java
+++ src/main/java/hj/project/token/services/Tokenizer.java
@@ -1,6 +1,30 @@
 package hj.project.token.services;
 
+import hj.project.token.configs.ConfigProperties;
+import hj.project.token.configs.TokenDb;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class Tokenizer implements MainTokenizer{
+
+    @Autowired
+    ConfigProperties configProp;
+
+    @Autowired
+    TokenDb tokenDb;
+
+    @PostMapping("/test")
+    public String getFoos(@RequestParam String id) {
+        System.out.println("ID: " + tokenDb.getDb()); //works
+        System.out.println(configProp.getConfigValue(("token.db"))); //works
+        return id;
+    }
 
     @Override
     public void init() {
@@ -10,4 +34,5 @@ public class Tokenizer implements MainTokenizer{
     public String returnToken(String value) {
         return null;
     }
+
 }
