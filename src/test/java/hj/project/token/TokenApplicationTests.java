8ca3f7509b708fc0d7b1b2dbac3d401810b19fbb
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-09-30 11:45:27 +1000

    updated test with a correct path to application-token

--- src/test/java/hj/project/token/TokenApplicationTests.java
+++ src/test/java/hj/project/token/TokenApplicationTests.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
 @RunWith( SpringJUnit4ClassRunner.class )
+@TestPropertySource(locations="classpath:application-token.properties")
 class TokenApplicationTests {
 
     @Autowired
